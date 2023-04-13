fddb688f615d54ff2c6275d5c06721916e2c5717
Sabrina Chaves <sabrinach08@gmail.com>
2023-04-13 20:43:13 -0300

    Implementa rotas do create e read

--- src/MoodMS/src/Networking.API/Controllers/MoodController.cs
+++ src/MoodMS/src/Networking.API/Controllers/MoodController.cs
@@ -20,7 +20,37 @@ public class MoodController : ControllerBase
     [Route("create")]
     public async Task<ActionResult<Mood>> CreateMood([FromBody] CreateMoodRequest request)
     {
+
             var mood = new Mood(request);
-            return await _moodStore.GetMood();
+            await _moodStore.CreateMood(mood);
+
+            return CreatedAtAction("GetMoodById", new { id = mood.Id }, mood);
+
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Mood>> GetMoodById(int id)
+    {
+        var mood = await _moodStore.GetMoodById(id);
+
+        if (mood == null)
+        {
+            return NotFound();
+        }
+
+        return mood;
+    }
+
+    [HttpGet("moods")]
+    public async Task<ActionResult<IEnumerable<Mood>>> GetMood(int id)
+    {
+        var mood = await _moodStore.GetMood();
+
+        if (mood == null)
+        {
+            return NotFound();
+        }
+
+        return mood;
     }
 }
