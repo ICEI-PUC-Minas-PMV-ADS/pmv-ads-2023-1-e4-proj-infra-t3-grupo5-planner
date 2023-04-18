2ec0b6bc2f925a581274bf462c2f8849b94037a4
palomacha100 <palomacha100@gmail.com>
2023-04-17 21:16:52 -0300

    Adiciona Page Repository

---
+++ src/PageMS/src/Store.PostgreSQL/Repositories/PageRepository.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Core.Stores;
+using Microsoft.EntityFrameworkCore;
+using Store.PostgreSQL.Database;
+
+namespace Store.PostgreSQL.Repositories;
+
+public class PageRepository : IPageStore
+{
+    private readonly Context _context;
+
+    public PageRepository(Context context)
+    {
+        _context = context;
+    }
+    public async Task CreatePage(Page page)
+    {
+        _context.Pages.Add(page);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Page> GetPageById(int id)
+    {
+        return await _context.Pages.FindAsync(id);
+    }
+
+    public async Task<List<Page>> GetPage()
+    {
+        return await _context.Pages.ToListAsync();
+    }
+}
