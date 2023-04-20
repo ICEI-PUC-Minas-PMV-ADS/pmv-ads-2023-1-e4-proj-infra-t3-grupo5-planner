c6547474b0ff10015cce60147a94c0b71397c312
Brunna Lage <brunnalage@gmail.com>
2023-04-19 23:46:16 -0300

    Feat: configuração e conexão do banco de dados para api do habitsMS

---
+++ src/HabitsMS/src/Store.PostgreSQL/Repositories/HabitRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Core.Stores;
+
+namespace Store.PostgreSQL.Repositories;
+
+public class HabitRepository : IHabitStore
+{
+    public Task<int> CreateHabit(Habit habit)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Habit> GetHabit()
+    {
+        throw new NotImplementedException();
+    }
+}
