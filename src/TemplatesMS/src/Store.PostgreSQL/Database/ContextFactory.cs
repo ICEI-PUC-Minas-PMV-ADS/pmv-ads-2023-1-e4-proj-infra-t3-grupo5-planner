06b45ac14886e7b7fdb758eafa8443e1fbd16e1d
Brunna Lage <brunnalage@gmail.com>
2023-04-23 14:34:23 -0300

    Feat: criação do ambiente da api TemplatesMS

---
+++ src/TemplatesMS/src/Store.PostgreSQL/Database/ContextFactory.cs
@@ -0,0 +1,27 @@
+using Store.PostgreSQL.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Store.PostgreSQL.Database;
+
+public class ContextFactory : IDesignTimeDbContextFactory<Context>
+{
+    public Context CreateDbContext(string[] args)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        
+        var configurationBuilder = new ConfigurationBuilder() 
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+        var optionsBuilder = new DbContextOptionsBuilder<Context>(); 
+        var connectionString = configurationBuilder.GetConnectionString("TemplateMSDatabase");
+        optionsBuilder.UseNpgsql(connectionString);
+        optionsBuilder.AddInterceptors(new SoftDeletableEntityInterceptor());
+
+        return new Context(optionsBuilder.Options);
+    }
+}
