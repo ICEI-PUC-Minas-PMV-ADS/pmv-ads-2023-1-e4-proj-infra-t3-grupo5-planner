6657d344955c455e425771145837a5c5c59359e8
palomacha100 <palomacha100@gmail.com>
2023-05-06 15:06:28 -0300

    Adiciona texto branco

--- frontend/src/components/Text/Text.styled.js
+++ frontend/src/components/Text/Text.styled.js
@@ -11,67 +11,54 @@ const TextStyled = styled.p`
 
   &.titulo-grande {
     font-size: ${toRem(32)};
-    width: 270px;
-    height: 51px;
     font-weight: 600;
   }
 
   &.item-widget {
     font-size: ${toRem(20)};
-    width: 328px;
-    height: 32px;
     font-weight: 400;
   }
 
   &.subtitulo {
     font-size: ${toRem(26)};
-    width: 328px;
-    height: 42px;
     font-weight: 600;
   }
 
   &.titulo-widget {
     font-size: ${toRem(20)};
-    width: 328px;
-    height: 32px;
     font-weight: 700;
     text-transform: uppercase;
   }
 
   &.label-styled {
     font-size: ${toRem(14)};
-    width: 328px;
-    height: 22px;
     font-weight: 700;
     color: #696767;
   }
 
-  &.texto-botao {
+  &.texto-botao-escuro,
+  &.texto-botao-claro {
     font-size: ${toRem(16)};
-    width: 95px;
-    height: 26px;
     font-weight: 700;
+  }
+
+  &.texto-botao-claro {
+    color: #ffffff;
   }
 
   &.texto-input {
     font-size: ${toRem(16)};
-    width: 328px;
-    height: 26px;
     font-weight: 400;
   }
 
   &.mensagem-erro {
     font-size: ${toRem(12)};
-    width: 328px;
-    height: 19px;
     font-weight: 400;
     color: #ed4b58;
   }
 
   &.titulo-pequeno {
     font-size: ${toRem(20)};
-    width: 328px;
-    height: 32px;
     font-weight: 600;
   }
 `;
