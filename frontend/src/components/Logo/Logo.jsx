ed52c7c156dbfe07588398c39d197cab76641c74
palomacha100 <palomacha100@gmail.com>
2023-05-06 17:13:17 -0300

    Inicia componente logo

---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TextStyled from './Logo.styled';
+import BlackLogo from '../../assets/logo-branca.png';
+
+const Logo = ({ blacklogo, whitelogo }) => {
+  return (
+    <LogoStyled>
+
+
+    </LogoStyled> 
+
+  );
+}
+
+export default Logo
