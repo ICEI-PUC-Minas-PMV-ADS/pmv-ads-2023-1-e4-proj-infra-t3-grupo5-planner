54f28f047535c47aaaadcd7cab26eac0943c7f97
palomacha100 <palomacha100@gmail.com>
2023-05-10 10:04:27 -0300

    inicia componente Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from "./components/Header/Header"
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+
+        <Header/>
+      </header>
     </div>
   );
 }
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Box from '../Box/Box'
+//import Logo from '../'
+import Button from '../Button/Button'
+
+const Header = () => {
+  return (
+    <Box id="HeaderBox"
+    content={
+        <>
+        <Button/>
+        </>
+    }></Box>
+  );
+}
+
+export default Header
