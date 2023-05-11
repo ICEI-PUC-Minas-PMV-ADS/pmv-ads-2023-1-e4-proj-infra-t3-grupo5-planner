504f3fcc6ca51898dff04489cd5cb591246e9eca
PedJump <pthyago212@gmail.com>
2023-05-10 23:25:49 -0300

    Criação do componente FormContent

---
+++ frontend/src/components/FormContent/FormContent.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const FormContentStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 8px 0px;
+`;
+export default FormContentStyled;
