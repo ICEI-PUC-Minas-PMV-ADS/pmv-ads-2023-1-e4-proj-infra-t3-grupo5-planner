e708a4a8b5dbe5e1e4afe588542be44b7719acca
palomacha100 <palomacha100@gmail.com>
2023-05-13 18:07:28 -0300

    Cria container Note

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header/Header";
 import "./index.css";
 import ContainerMood from "./components/ContainerMood/ContainerMood";
+import ContainerNote from "./components/ContainerNote/ContainerNote";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
         <ContainerMood></ContainerMood>
+        <ContainerNote />
       </header>
     </div>
   );
