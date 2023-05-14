f4696ad1594ef96fd1a8bec8d377052b3b2b9a7b
sabrinachaves <sabrinach08@gmail.com>
2023-05-14 12:12:16 -0300

    Adiciona o router para a página de ajuda no header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import "./index.css";
-import ContainerReminder from "./components/ContainerReminder/ContainerReminder";
-import ContainerFinance from "./components/ContainerFinance/ContainerFinance";
-import ContainerChecklist from "./components/ContainerChecklist/ContainerChecklist";
-import ContainerNote from "./components/ContainerNote/ContainerNote";
-import { WidgetHabits } from "./components/WidgetHabits/WidgetHabits";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PageAjuda from "./components/PageAjuda/PageAjuda";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header />
-      </header>
-      <ContainerReminder></ContainerReminder>
-      <ContainerFinance></ContainerFinance>
-      <ContainerChecklist></ContainerChecklist>
-      <ContainerNote></ContainerNote>
-      <WidgetHabits></WidgetHabits>
+      <Router>
+        <header className="App-header">
+          <Header />
+        </header>
+
+        <Routes>
+          <Route path="/ajuda" element={<PageAjuda />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,32 +3,33 @@ import Box from '../Box/Box'
 import Logo from '../Logo/Logo'
 import Button from '../Button/Button'
 import { HeaderContainer, NavMenu, Anchor } from './Header.styled'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <HeaderContainer>
-        <Box 
-        id="HeaderBox"
-        content={
-            <>
-                <Logo whiteLogo heightImage={48}/>
-                <NavMenu>
-                    <Anchor href="Home">
-                        <Button herf="Home" id="header-button" textId="button-red-text" text="Início"/>
-                    </Anchor>
-                    <Anchor href="Agenda">
-                        <Button id="header-button" textId="button-red-text" text="Agenda"/>
-                    </Anchor>
-                    <Anchor href="Ajuda">
-                       <Button id="header-button" textId="button-red-text" text="Ajuda"/>
-                    </Anchor>
-                    
-                </NavMenu>
-                <Button profileIcon id="profile-picture" text=""/>
-            </>            
-        }></Box>
-    </HeaderContainer>
-  );
+    return (
+        <HeaderContainer>
+            <Box
+                id="HeaderBox"
+                content={
+                    <>
+                        <Logo whiteLogo heightImage={48} />
+                        <NavMenu>
+                            <Anchor href="Home">
+                                <Button herf="Home" id="header-button" textId="button-red-text" text="Início" />
+                            </Anchor>
+                            <Anchor href="Agenda">
+                                <Button id="header-button" textId="button-red-text" text="Agenda" />
+                            </Anchor>
+                            <Link to="/ajuda" style={{ textDecoration: 'none' }}>
+                                <Button id="header-button" textId="button-red-text" text="Ajuda" />
+                            </Link>
+                        </NavMenu>
+
+                        <Button profileIcon id="profile-picture" text="" />
+                    </>
+                }></Box>
+        </HeaderContainer>
+    );
 }
 
 export default Header
