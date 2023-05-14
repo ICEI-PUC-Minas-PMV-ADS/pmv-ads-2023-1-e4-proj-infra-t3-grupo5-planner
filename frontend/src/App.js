eb7093384dd002dde6543f70c63a622d5eff2e64
PedJump <pthyago212@gmail.com>
2023-05-14 18:38:51 -0300

    Finalização da tela edição de perfil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import "./index.css";
-import ContainerReminder from "./components/ContainerReminder/ContainerReminder";
-import ContainerFinance from "./components/ContainerFinance/ContainerFinance";
-import ContainerChecklist from "./components/ContainerChecklist/ContainerChecklist";
-import ContainerNote from "./components/ContainerNote/ContainerNote";
-import { WidgetHabits } from "./components/WidgetHabits/WidgetHabits";
+import PageProfileEdit from "./components/PageProfileEdit/PageProfileEdit";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
-      <ContainerReminder></ContainerReminder>
-      <ContainerFinance></ContainerFinance>
-      <ContainerChecklist></ContainerChecklist>
-      <ContainerNote></ContainerNote>
-      <WidgetHabits></WidgetHabits>
+      <PageProfileEdit />
     </div>
   );
 }
