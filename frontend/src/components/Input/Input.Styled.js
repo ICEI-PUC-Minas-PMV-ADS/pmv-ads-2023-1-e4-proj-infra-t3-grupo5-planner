487176ab45999a8c05aaaad18b5054a4e7fbe605
Brunna Lage <brunnalage@gmail.com>
2023-05-14 18:38:05 -0300

    Feat: adicionando os estilos do case select

--- frontend/src/components/Input/Input.Styled.js
+++ frontend/src/components/Input/Input.Styled.js
@@ -29,3 +29,16 @@ export const InputStyled = styled.input`
     cursor: pointer;
   }
 `;
+
+export const SelectStyled = styled.select`
+  width: 100%;
+  background: #ffffff;
+  border: ${toRem(2)} solid #333333;
+  border-radius: ${toRem(8)};
+  padding: ${toRem(4)} ${toRem(8)};
+  font-family: "Quicksand";
+  font-style: normal;
+  font-weight: 400;
+  font-size: ${toRem(16)};
+  color: #333333;
+  `;
