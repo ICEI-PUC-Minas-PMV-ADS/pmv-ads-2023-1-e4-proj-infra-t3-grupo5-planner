ade19e9f15dee85baa4cd1c6466c3d9a8fca22ee
PedJump <pthyago212@gmail.com>
2023-05-14 22:49:31 -0300

    Teste

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header/Header";
 import "./index.css";
 import PageHome from "./components/PageHome/PageHome";
-
 function App() {
   return (
     <div className="App">
