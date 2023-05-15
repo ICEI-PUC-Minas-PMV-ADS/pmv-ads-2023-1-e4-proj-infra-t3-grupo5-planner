0e30705587baa1e6b87069b58bca70ed4ee67bd9
PedJump <pthyago212@gmail.com>
2023-05-14 23:23:24 -0300

    Resolvendo conflitos da PageHome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import "./index.css";
-import PageProfileEdit from "./components/PageProfileEdit/PageProfileEdit";
+import PageHome from "./components/PageHome/PageHome";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
-<<<<<<< HEAD
       <PageHome />
-=======
-      <PageProfileEdit />
->>>>>>> 3c70970fb1f8893cc713793d1104b50b23fb70ed
     </div>
   );
 }
