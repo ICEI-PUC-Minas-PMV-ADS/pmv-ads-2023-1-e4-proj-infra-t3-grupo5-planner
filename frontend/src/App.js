d7f9795e7904b7b70812f0b35dbd7e21fb09342a
PedJump <pthyago212@gmail.com>
2023-05-14 22:55:56 -0300

    Limpando o app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import Header from "./components/Header/Header";
 import "./index.css";
 import PageHome from "./components/PageHome/PageHome";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
-      <PageHome />
     </div>
   );
 }
