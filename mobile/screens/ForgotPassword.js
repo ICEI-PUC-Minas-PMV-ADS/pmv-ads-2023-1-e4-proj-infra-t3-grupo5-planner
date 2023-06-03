8022e6d966eede686ef17a08560293725c697425
sabrinachaves <sabrinach08@gmail.com>
2023-06-03 13:37:10 -0300

    Cria página de "Esqueci minha senha"

--- mobile/screens/ForgotPassword.js
+++ mobile/screens/ForgotPassword.js
@@ -1,9 +1,33 @@
-import {SafeAreaView, Text} from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
+import Button from '../components/Buttons';
+import AppText from '../components/AppText';
+import ImgEmail from '../assets/email.png';
+import InputLabel from '../components/Inputs';
+import ScreenWrapper from '../components/ScreenWrapper';
+import ItemSeparator from '../components/ItemSeparator';
 
 const ForgotPassword = () => (
-    <SafeAreaView>
-        <Text>ForgotPassword</Text>
-    </SafeAreaView>
+    <ScreenWrapper style={style.container}>
+        <Image source={ImgEmail} style={style.ImgSize} />
+        <AppText type="Subtitle">Insira o endereço de e-mail associado à sua conta MyPlan para enviarmos um link de redefinição</AppText>
+        <ItemSeparator size="l" />
+        <InputLabel placeholder="Digite seu e-mail" labelText="E-mail" errorMessage="Digite um e-mail válido" />
+        <ItemSeparator size="l" />
+        <Button title="Enviar e-mail" />
+    </ScreenWrapper>
 );
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    ImgSize: {
+        width: 204,
+        height: 206,
+    }
+})
 
 export default ForgotPassword;
