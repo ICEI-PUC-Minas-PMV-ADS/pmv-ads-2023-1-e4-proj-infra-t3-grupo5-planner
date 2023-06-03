a3a9f6f84be830fa5fd74324bfd81141ab1d158d
Davison Cavalcante <dcbarros1@gmail.com>
2023-06-03 19:08:34 -0300

    Inicio da Lógica da Tela de Login

--- mobile/screens/Login.js
+++ mobile/screens/Login.js
@@ -1,4 +1,8 @@
-import {StyleSheet} from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+
 import Button from '../components/Buttons'
 import InputLabel from '../components/Inputs';
 import ItemSeparator from '../components/ItemSeparator';
@@ -6,37 +10,60 @@ import ScreenWrapper from '../components/ScreenWrapper';
 import Logo from '../components/Logo';
 import AppText from '../components/AppText';
 
-
-const LoginSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('Endereço de e-mail inválido.')
-      .required('Preencha este campo.'),
-    password: Yup.string().required('Preencha este campo.'),
+const loginSchema = Yup.object().shape({
+    email: Yup.string().email('Email inválido').required('Campo obrigatório'),
+    password: Yup.string().required('Campo obrigatório'),
 });
 
-const Login = ({ navigation }) => (
-
-    <ScreenWrapper style={styles.loginContainer}>
+const Login = ({ navigation }) => {
     
-        <Logo blackLogo size='m'/>
-        <ItemSeparator size={24}/>
-        <InputLabel labelText="Email" labelStyle="standard" />
-        <ItemSeparator size={8}/>
-        <InputLabel labelText="Senha" labelStyle="standard" MaskedInput="password"/>
-        <ItemSeparator size={64}/>
-        <Button title="Entrar" fontColor='white'/>
-        <ItemSeparator size={24}/>
-        <AppText type='MediumTextBold'> Não tem registro? </AppText>
-        <Button title="Registrar" buttonType="transparentStandard" fontColor={"black"} onPress={() => navigation.navigate('Register')}/>
+    const [errorMessage, setErrorMessage] = useState('');
 
-    </ScreenWrapper>
-);
+    const handleLogin = (values) => {
+        if (!values.email) {
+            setErrorMessage('Por favor, preencha o campo de email');
+            return;
+        }
+        if (!values.password) {
+            setErrorMessage('Por favor, preencha o campo de senha');
+            return;
+        }
+        
+        // Lógica de login
+        
+    };
+    
+    return (
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={loginSchema}
+            onSubmit={handleLogin}
+        >
+            {({ handleSubmit, values, handleChange }) => (
+                <ScreenWrapper style={styles.loginContainer}>
+                    <Logo blackLogo size='m' />
+                    <ItemSeparator size={24} />
+                    <InputLabel labelText="Email" labelStyle="standard" errorMessage={errorMessage}/>
+                    <ItemSeparator size={8} />
+                    <InputLabel labelText="Senha" labelStyle="standard" MaskedInput="password" errorMessage={errorMessage}/>
+                    <ItemSeparator size={48} />
+                    <Button title="Entrar" fontColor='white' onPress={() => handleSubmit()} />
+                    <Button title="Esqueci a senha" buttonType="transparentStandard" fontColor={"black"} />
+                    <ItemSeparator size={24} />
+                    <AppText type='MediumTextBold'> Não tem registro? </AppText>
+                    <Button title="Registrar" buttonType="transparentStandard" fontColor={"black"} onPress={() => navigation.navigate('Register')} />
+                </ScreenWrapper>
+            )}
+        </Formik>
+    );
+};
 
 const styles = StyleSheet.create({
     loginContainer: {
         justifyContent: 'center',
-        alignItems:'center',
-        flexDirection:'column',
+        alignItems: 'center',
+        flexDirection: 'column',
     },
 });
+
 export default Login;
