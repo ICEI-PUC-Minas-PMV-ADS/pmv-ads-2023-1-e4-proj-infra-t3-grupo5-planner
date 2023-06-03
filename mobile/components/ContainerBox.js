a4f9e9615b758dea0b8b7a158d20135f3e41076e
PedJump <pthyago212@gmail.com>
2023-06-03 12:49:53 -0300

    Implementação do color no componente AppText

--- mobile/components/ContainerBox.js
+++ mobile/components/ContainerBox.js
@@ -1,52 +1,56 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import AppText from "./AppText";
 
-const ContainerBox = ({
-  title, content
-}) => {
+const ContainerBox = ({ title, content }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.redBox}><Text>{title}</Text></View>
-      <View style={styles.greyBox}><>{content}</></View>
+      <View style={styles.redBox}>
+        <AppText type="Title" color="#ffffff">
+          {title}
+        </AppText>
+      </View>
+      <View style={styles.greyBox}>
+        <>{content}</>
+      </View>
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
-
   container: {
     width: 312,
     height: 524,
     borderRadius: 16,
     borderWidth: 2,
-    borderColor: '#000',
-    position: 'relative',
-    alignSelf: 'center',
-    backgroundColor: '#F2F2F2',
+    borderColor: "#000",
+    position: "relative",
+    alignSelf: "center",
+    backgroundColor: "#F2F2F2",
   },
 
   redBox: {
-    position: 'absolute',
+    position: "absolute",
     top: -2,
     left: -2,
     width: 312,
     height: 56,
-    backgroundColor: '#ED4B58',
+    backgroundColor: "#ED4B58",
     borderWidth: 2,
-    borderColor: '#000',
+    borderColor: "#000",
     borderTopLeftRadius: 16,
     borderTopRightRadius: 16,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "flex-start",
+    paddingLeft: 24,
   },
 
   greyBox: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default ContainerBox;
--- mobile/components/Inputs.js
+++ mobile/components/Inputs.js
@@ -1,114 +1,118 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import MaskInput, { Masks } from 'react-native-mask-input'
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import MaskInput, { Masks } from "react-native-mask-input";
+import AppText from "./AppText";
 
-const InputLabel = ({labelText, placeholder, inputSize = 280 , icon, errorMessage, labelStyle, MaskedInput, ...props}) => {
-    
-    const [masked, setMasked] = React.useState('');
+const InputLabel = ({
+  labelText,
+  placeholder,
+  inputSize = 280,
+  icon,
+  errorMessage,
+  labelStyle,
+  MaskedInput,
+  ...props
+}) => {
+  const [masked, setMasked] = React.useState("");
 
-    const getInputStyle = () => {
-        switch (labelStyle){
-            case 'textArea':
-                return styles.textArea;
-            default:
-                return styles.standard;
-        }
+  const getInputStyle = () => {
+    switch (labelStyle) {
+      case "textArea":
+        return styles.textArea;
+      default:
+        return styles.standard;
     }
-    const getIcon = () => {
-        switch (icon){
-            case "schedule":
-                return "calendar-month"
-            case "clock":
-                return "clock-outline"
-            case "checkbox":
-                return "checkbox-outline"
-        }
+  };
+  const getIcon = () => {
+    switch (icon) {
+      case "schedule":
+        return "calendar-month";
+      case "clock":
+        return "clock-outline";
+      case "checkbox":
+        return "checkbox-outline";
     }
-    const getMask = () => {
-        switch(MaskedInput){
-            case "date":
-                return Masks.DATE_DDMMYYYY
-            case "hour":
-                return [/\d/, /\d/, ':', /\d/, /\d/]
-            case "expense":
-                return Masks.BRL_CURRENCY
-        }
+  };
+  const getMask = () => {
+    switch (MaskedInput) {
+      case "date":
+        return Masks.DATE_DDMMYYYY;
+      case "hour":
+        return [/\d/, /\d/, ":", /\d/, /\d/];
+      case "expense":
+        return Masks.BRL_CURRENCY;
     }
+  };
 
-    const textAreaStyled = labelStyle === 'textArea' ? {padding: 10} : {};
+  const textAreaStyled = labelStyle === "textArea" ? { padding: 10 } : {};
 
-    return(
-        <View style={props.style}>
-            {labelText && <Text style={styles.labelText}>{labelText}</Text>}
-            <View style={styles.container}>
-                <MaskInput 
-                    value={masked}                    
-                    onChangeText={(mask) => setMasked(mask)} 
-                    mask={getMask()} 
-                    secureTextEntry = {MaskedInput === "password" ? true : false}
-                    style={[getInputStyle(), {width: inputSize}, textAreaStyled]} 
-                    inputMode={MaskedInput === 'date' || MaskedInput === 'hour' ? 'numeric' : 'none'}
-                    placeholder={placeholder}
-                    multiline={labelStyle === 'textArea' ? true : false}
-                    numberOfLines={labelStyle === 'textArea' ? 10 : null}
-                    textAlignVertical={labelStyle === 'textArea' ? "top" : null}
-                />
-                {icon && <MaterialCommunityIcons name={getIcon()} size={24} color="black" style={styles.icon}/>}
-            </View>
-            <Text style={styles.errorText}>{errorMessage}</Text>
-        </View>
-    );
-}
+  return (
+    <View style={props.style}>
+      {labelText && <AppText type="Label">{labelText}</AppText>}
+      <View style={styles.container}>
+        <MaskInput
+          value={masked}
+          onChangeText={(mask) => setMasked(mask)}
+          mask={getMask()}
+          secureTextEntry={MaskedInput === "password" ? true : false}
+          style={[getInputStyle(), { width: inputSize }, textAreaStyled]}
+          inputMode={
+            MaskedInput === "date" || MaskedInput === "hour"
+              ? "numeric"
+              : "none"
+          }
+          placeholder={placeholder}
+          multiline={labelStyle === "textArea" ? true : false}
+          numberOfLines={labelStyle === "textArea" ? 10 : null}
+          textAlignVertical={labelStyle === "textArea" ? "top" : null}
+        />
+        {icon && (
+          <MaterialCommunityIcons
+            name={getIcon()}
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+        )}
+      </View>
+      <AppText type="ErrorText" color="#ED4B58">
+        {errorMessage}
+      </AppText>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-    
-
-    standard:{
-        height:40,
-        borderRadius:8,
-        paddingVertical:4,
-        paddingHorizontal:8,
-        borderWidth:2,
-        borderColor:'#333333',
-        /*fontFamily:"Quicksand_400Regular",*/
-        fontStyle:"normal"
-    },
-    textArea:{
-        width:280,
-        height:'200%',
-        borderWidth:2,
-        borderColor:'#333333',
-        borderRadius:8,
-        backgroundColor:"white",
-        /*fontFamily:"Quicksand_400Regular",*/
-        fontStyle:"normal",
-        padding: 10
-    },
-    labelText:{
-        /*fontFamily:"Quicksand_700Bold",*/
-        fontStyle:"normal",
-        fontSize: 14,
-        color: "#696767",
-    },
-    errorText:{
-        /*fontFamily:"Quicksand_400Regular",*/
-        fontStyle:"normal",
-        fontSize:12,
-        color:'#ED4B58',
-        marginTop:0
-    },  
-    container:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 5,
-    },
-    icon: {
-        position: 'absolute',
-        top: 10,
-        right: 10,
-        zIndex: 1,
-    },
+  standard: {
+    height: 40,
+    borderRadius: 8,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderWidth: 2,
+    borderColor: "#333333",
+    fontStyle: "normal",
+  },
+  textArea: {
+    width: 280,
+    height: "200%",
+    borderWidth: 2,
+    borderColor: "#333333",
+    borderRadius: 8,
+    backgroundColor: "white",
+    fontStyle: "normal",
+    padding: 10,
+  },
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  icon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    zIndex: 1,
+  },
 });
 export default InputLabel;
