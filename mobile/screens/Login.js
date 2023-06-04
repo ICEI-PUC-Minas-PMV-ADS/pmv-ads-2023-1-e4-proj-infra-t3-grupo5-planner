77c71f70880319ef848ebc025ad4c83474a10c7a
palomacha100 <palomacha100@gmail.com>
2023-06-04 10:32:35 -0300

    Resolve conflitos entre branchs

--- mobile/screens/Login.js
+++ mobile/screens/Login.js
@@ -1,20 +1,27 @@
-import {StyleSheet,SafeAreaView} from 'react-native';
-import Button from '../components/Buttons'
-import InputLabel from '../components/Inputs';
-import ContainerBox from '../components/ContainerBox';
-import Header from '../components/Header';
+import { StyleSheet, SafeAreaView } from "react-native";
+import Button from "../components/Buttons";
+import InputLabel from "../components/Inputs";
+import ContainerBox from "../components/ContainerBox";
+import Header from "../components/Header";
 
 const Login = ({ navigation }) => (
-    <SafeAreaView style={styles.container}>
-        <Header />
-    </SafeAreaView>
+  <SafeAreaView style={styles.container}>
+    {/*
+            <Text>Login</Text>
+            <InputLabel labelText="Email" labelStyle="standard" />
+            <InputLabel labelText="Senha" labelStyle="standard" MaskedInput="password"/>
+            <Button title="Entrar" />
+            <Button title="Registrar" buttonType="transparentStandard" fontColor={"black"} onPress={() => navigation.navigate('Register')}/>
+        */}
+    <Header />
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
-    container:{
-        padding:150,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  container: {
+    padding: 150,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 export default Login;
