a3c1a0875a9331dc88ea54ece8bc5bedf1aeae2b
Davison Cavalcante <dcbarros1@gmail.com>
2023-06-04 23:31:56 -0300

    Modificação do componente ModalReminder

---
+++ mobile/components/ModalContainer.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import AppText from "./AppText";
+
+const ModalContainer = ({ title, content}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.redBox}>
+        <AppText type="Title" color="#ffffff">
+          {title}
+        </AppText>
+      </View>
+      <View style={styles.greyBox}>{content}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "flex-end",
+    bottom: 0,
+    left: 0,
+    width: "100%",
+    flex: 1,
+  },
+
+  redBox: {
+    width: "100%",
+    height: 56,
+    backgroundColor: "#ED4B58",
+    borderWidth: 2,
+    borderColor: "#000",
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    paddingLeft: 24,
+  },
+
+  greyBox: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    width: "100%",
+    borderTopWidth: 0,
+    borderBottomLeftRadius: 16,
+    borderBottomRightRadius: 16,
+    borderWidth: 2,
+    borderColor: "#000",
+    backgroundColor: "#F2F2F2",
+  },
+});
+
+export default ModalContainer;
--- mobile/components/ModalReminder.js
+++ mobile/components/ModalReminder.js
@@ -1,112 +1,38 @@
-import React, { useState } from 'react';
-import { View, Modal, StyleSheet, KeyboardAvoidingView} from 'react-native';
+import React from 'react';
+import { View, Modal, StyleSheet} from 'react-native';
 import Button from './Buttons';
-import AppText from './AppText'
 import InputLabel from './Inputs';
 import ItemSeparator from './ItemSeparator';
+import ModalContainer from './ModalContainer'
 
-const ModalReminder = () => {
+const ModalReminder = (onPressAdd, onPressReturn) => {
 
-
-    const [modalVisible, setModalVisible] = useState(false);
-
-    const toggleModal = () => {
-        setModalVisible(!modalVisible);
-    };
-
-    return (
-        <KeyboardAvoidingView
-        style={styles.modalContainer}
-        behavior="padding"
-        enabled
-        >
-            <Button title="Abrir Modal" onPress={toggleModal} />
-                <Modal
-                    visible={modalVisible}
-                    animationType="fade"
-                    transparent={true}
-                    onRequestClose={toggleModal}
-                >
-
-                    <View style={styles.modalContent}>
-                            <View style={styles.redBox}>
-                                <AppText type="Title" color="#ffffff">
-                                    Defina um lembrete
-                                </AppText>
-                            </View>
-                            <View style={styles.greyBox}>
-                                <View style={styles.Boxcontainer}>
-                                    <InputLabel labelText="Lembrete" inputSize="100%" placeholder="Digite o seu lembrete"/>
-                                    <View style={styles.dateTime}>
-                                        <InputLabel labelText="Dia" inputSize={154} placeholder="__/__/____" icon='schedule' MaskedInput='date'/>
-                                        <ItemSeparator size="s"/>
-                                        <InputLabel labelText="Hora" inputSize={123} placeholder="__:__" icon='clock' MaskedInput='hour'/>
-                                    </View>
-                                    <ItemSeparator size="l"/>
-                                    <Button title="Adicionar Lembrete" fontColor='#ffffff'/>
-                                    <Button title="Cancelar" buttonType="transparentStandard" onPress={toggleModal} fontColor='#333333' style={{width: "100%"}}/>
-                                </View>
-                            </View>
-                    </View>
-                </Modal>
-        </KeyboardAvoidingView>
-        
+    return(
+        <ModalContainer title='Definir um lembrete' content={      
+            <View style={{width:'100%'}}>
+                <InputLabel labelText="Lembrete" inputSize="100%" placeholder="Digite o seu lembrete"/>
+                <View style={styles.boxLine}>
+                    <InputLabel labelText="Dia" inputSize={175} placeholder="__/__/____" icon='schedule' MaskedInput='date'/>
+                    <ItemSeparator size="s"/>
+                    <InputLabel labelText="Hora" inputSize={123} placeholder="__:__" icon='clock' MaskedInput='hour'/>
+                </View>
+                <ItemSeparator size="l"/>
+                <Button title="Adicionar Lembrete" fontColor='#ffffff' onPress={onPressAdd} style={{width: "100%"}}/>
+                <Button title="Cancelar" buttonType="transparentStandard" fontColor='#333333' onPress={onPressReturn} style={{width: "100%"}}/>
+            </View>
+        }/>
     );
-};
-
-
-
+}
 const styles = StyleSheet.create({
-    Boxcontainer:{
-        display: "flex",
-        flex: 1, 
-        flexDirection: "column",
+
+    boxLine:{
+        flexDirection:'row'
+    },
+    modalContainer:{
+        width:'100%',
         alignContent:'center',
         justifyContent:'center',
-        width:"80%",
-    },
-    modalContainer: {
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: 'flex-end',
-    },
-    modalContent: {
-        display: "flex",
-        flex: 1, 
-        backgroundColor:'rgba(217, 217, 217, 0.6)',
-        justifyContent: 'flex-end', 
-        alignItems: 'center',
-        flexDirection: "column",
-        height: "auto",
-    },
-    redBox: {
-        width: '100%',
-        height: 56,
-        backgroundColor: "#ED4B58",
-        borderWidth: 2,
-        borderColor: "#000",
-        borderTopLeftRadius: 16,
-        borderTopRightRadius: 16,
-        justifyContent: "center",
-        alignItems: "flex-start",
-        paddingLeft: 24,
-    },
-    greyBox: {
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 16,
-        height: '50%',
-        width: '100%',
-        borderTopWidth: 0,
-        borderWidth: 0,
-        borderColor: "#000",
-        alignSelf: "center",
-        backgroundColor: "#F2F2F2",
-    },
-    dateTime: {
-        flexDirection:'row'
 
     }
 });
-
 export default ModalReminder;
